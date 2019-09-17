c28b13f97d39faf3200e74eb74a6534548faac68
xexxy <kratiukartiom@gmail.com>
2019-09-17 11:18:50 +0300

    Add main slider

--- js/script.js
+++ js/script.js
@@ -2,39 +2,54 @@ function showSlides(id) {
 
   // debugger
 
-  var slideIndex = 0;
-  var slides = document.querySelectorAll(`${id} .item`);
-  var arrows = document.querySelectorAll(`${id} .arrow`);
-  var dots = document.querySelectorAll(`${id} .slider-dots-item`);
+  let slideIndex = 0;
+  let slides = document.querySelectorAll(`${id} .item`);
+  let arrows = document.querySelectorAll(`${id} .arrow`);
+  let dots = document.querySelectorAll(`${id} .slider-dots-item`);
+  let interval;
 
-  if (slides.length > 0) {
-    slides[0].style.display = "block";
+  if (slides.length) {
+    slides[0].style.display = 'block';
 
     for (let i = 0; i < dots.length; i++) {
-      dots[i].addEventListener("click", function () {
+      dots[i].addEventListener('click', function () {
         setIndex(i)
         removeActive()
         setActive()
+        updateInterval()
       })
     }
   }
 
-  if (dots.length > 0) {
-    dots[0].className += " active";
+  if (dots.length) {
+    dots[0].className += ' active';
   }
 
-  if (arrows.length > 0) {
-    arrows[0].addEventListener("click", function () {
+  if (arrows.length) {
+    arrows[0].addEventListener('click', function () {
       decreaseIndex()
       removeActive()
       setActive()
+      updateInterval()
     })
 
-    arrows[1].addEventListener("click", function () {
+    arrows[1].addEventListener('click', function () {
       increaseIndex()
       removeActive()
       setActive()
+      updateInterval()
     })
+  }
+
+  updateInterval();
+
+  function updateInterval() {
+    clearInterval(interval);
+    interval = setInterval(function () {
+      increaseIndex()
+      removeActive()
+      setActive()
+    }, 10000);
   }
 
   function increaseIndex() {
@@ -56,17 +71,51 @@ function showSlides(id) {
   }
 
   function setActive() {
-    slides[slideIndex].style.display = "block";
-    dots[slideIndex].className += " active";
+    slides[slideIndex].style.display = 'block';
+    dots[slideIndex].className += ' active';
   }
 
   function removeActive() {
     for (let s = 0; s < slides.length; s++) {
-      slides[s].style.display = "none";
+      slides[s].style.display = 'none';
     }
     for (let d = 0; d < dots.length; d++) {
-      dots[d].classList.remove("active");
-      // dots[d].className = dots[d].className.replace(" active", "");
+      dots[d].classList.remove('active');
     }
   }
+}
+
+function shiftSlide() {
+  let slider = document.querySelector('.nav-features');
+  let slides = document.querySelectorAll('.item');
+  let buttons = document.querySelectorAll('.button');
+  let i = 0;
+
+  buttons[i].style.marginLeft = 0;
+
+  setInterval(function () {
+
+    if (i === slides.length - 1) {
+      i = 0;
+      slider.style.transform = 'translateX(0)';
+      for (let index of slides) {
+        index.style.opacity = 1;
+      }
+      setTimeout(function () {
+        for (let index = 1; index < buttons.length; index++) {
+          buttons[index].style.marginLeft = '400px';
+        }
+      }, 1500)
+    } else {
+      for (let index of slides) {
+        index.style.opacity = 0;
+      }
+      i++;
+      slides[i].style.opacity = 1;
+      slider.style.transform = `translateX(-${i * 400}px)`;
+      buttons[i].style.marginLeft = 0;
+      
+    }
+
+  }, 5000);
 }
