d3e6746ebaf1cf9c169daf8af821e8215872f71c
xexxy <38164075+xexxy@users.noreply.github.com>
2022-04-07 23:28:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -184,14 +184,14 @@ const prices = document.querySelectorAll('.main-services__price');
 input.addEventListener('input', countArea);
 radios.forEach(item => item.addEventListener('change', countArea));
 
-const basisArea = 150;
+const basisArea = 180;
 
 function countArea() {
   if (input.value > 400) input.value = 400;
   if (input.value < 0) input.value = 0;
 
   let area = input.value;
-  let k1 = radioOne.checked ? 1 : radioTwo.checked ? 1.15 : 1.3;
+  let k1 = radioOne.checked ? 1 : radioTwo.checked ? 1.25 : 1.3;
   let k2 = radioConstruction.checked ? 1 : 1.3;
 
   if (area > basisArea) {
@@ -199,7 +199,7 @@ function countArea() {
     prices[1].innerText = `${Math.ceil((basisArea * 12 + (area - basisArea) * basisArea / area * 12) * k1 * k2 / 10) * 10} BYN`;
     prices[2].innerText = `${Math.ceil((basisArea * 38 + (area - basisArea) * basisArea / area * 38) * k1 * k2 / 10) * 10} BYN`;
   } else if (area > 0) {
-    if (area < 75) area = 75;
+    if (area < 90) area = 90;
     prices[0].innerText = `${Math.ceil((basisArea * 7 + (area - basisArea) * area / basisArea * 7) * k1 * k2 / 10) * 10} BYN`;
     prices[1].innerText = `${Math.ceil((basisArea * 12 + (area - basisArea) * area / basisArea * 12) * k1 * k2 / 10) * 10} BYN`;
     prices[2].innerText = `${Math.ceil((basisArea * 38 + (area - basisArea) * area / basisArea * 38) * k1 * k2 / 10) * 10} BYN`;
