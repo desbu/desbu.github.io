30a3cfdee2a201653e49fecb15fe0419ba8c1ae5
xexxy <38164075+xexxy@users.noreply.github.com>
2022-07-26 20:50:47 +0300

    Fix main-slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,14 @@
 const swiper = new Swiper('.swiper', {
 	pagination: {
 		el: '.swiper-pagination',
+		clickable: true,
 	},
 	autoplay: {
 		delay: 3000,
 	},
 	speed: 1000,
 	loop: true,
+	keyboard: {
+		enabled: true,
+	},
 });
