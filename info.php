895a8b59d85b804bc5ac031b7c51eb03154ad6ec
xexxy <38164075+xexxy@users.noreply.github.com>
2023-08-15 00:18:37 +0300

    Update info.php

--- info.php
+++ info.php
@@ -3,8 +3,11 @@
 $api_key = 'AIzaSyBhK7mm-ALShJfQPLvmRepTEXtZf_B00zk';
 $place_id = 'ChIJqz8fdX4PIUcRXGacHrrSQF4';
 
-echo "https://maps.googleapis.com/maps/api/place/details/json?placeid={$place_id}&fields=name,rating,formatted_phone_number,reviews&key={$api_key}"; 
+$url = "https://maps.googleapis.com/maps/api/place/details/json?placeid={$place_id}&fields=name,rating,formatted_phone_number,reviews&key={$api_key}"; 
 
-echo "qwe{$a}rty";
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $url);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec ($ch);
 
 ?>
