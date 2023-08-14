a2bdc3925525d5cea4fa8176e04b513022f0fc3c
xexxy <38164075+xexxy@users.noreply.github.com>
2023-08-14 23:57:27 +0300

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
+
+$api_key = 'AIzaSyBhK7mm-ALShJfQPLvmRepTEXtZf_B00zk';
+$place_id = 'ChIJqz8fdX4PIUcRXGacHrrSQF4';
+
+echo "https://maps.googleapis.com/maps/api/place/details/json?placeid={$place_id}&fields=name,rating,formatted_phone_number,reviews&key={$api_key}"; 
+
+echo "qwe{$a}rty";
+
 ?>
