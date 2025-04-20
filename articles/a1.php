f3e95c39117893d749dda340bc0971830cf57e57
xexxy <38164075+xexxy@users.noreply.github.com>
2025-04-20 21:30:03 +0300

    Refactor

---
+++ articles/a1.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description"
+		content="С чего начать строительство дома. Получение разрешения на проектирование и строительство" />
+	<meta name="keywords" content="Дом, коттедж, земельный участок" />
+
+	<meta property="og:title" content="Хотим свой дом! С чего начать? (1 часть)" />
+	<meta property="og:description"
+		content="С чего начать строительство дома. Получение разрешения на проектирование и строительств" />
+	<meta property="og:image" content="https://marchuk.by/articles/a1-1.jpg" />
+
+	<title>Хотим свой дом! С чего начать? (1 часть)</title>
+
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152934332-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-152934332-1');
+	</script>
+</head>
+
+<body class="secondary-page">
+
+	<header>
+		<nav>
+			<a class="nav-logo" href="/">
+				<h1><span class="hidden-logo">MARCHUK | </span>Строительные проекты</h1>
+			</a>
+			<ul class="main-nav">
+				<li class="link"><a href="/">Главная</a></li>
+				<div class="line-nav"></div>
+				<li class="link"><a href="/#about-us">О нас</a></li>
+				<li class="link"><a href="/#services">Услуги</a></li>
+				<li class="link"><a href="/#articles">Статьи</a></li>
+				<li class="link"><a href="/#contacts">Контакты</a></li>
+			</ul>
+			<div class="nav-button">
+				<span></span>
+			</div>
+			<div class="overlay"></div>
+		</nav>
+	</header>
+
+	<main>
+		<article>
+			<div class="wrapper">
+				<h2>Хотим свой дом! С чего начать? (1&nbsp;часть)</h2>
+			</div>
+			<p>Вы&nbsp;хотите свой собственный уютный дом? Добро пожаловать на&nbsp;цикл статей &laquo;Хотим свой дом!
+				С&nbsp;чего начать?&raquo;. Это краткий ликбез по&nbsp;марафону от&nbsp;задумки
+				до&nbsp;ее&nbsp;физического воплощения в&nbsp;виде собственного уютного жилища.</p>
+			<figure>
+				<img class="center" src="a1-1.jpg" loading="lazy" alt="Свой уютный дом">
+				<figcaption>Свой уютный дом</figcaption>
+			</figure>
+			<p>В&nbsp;этой части цикла статей мы&nbsp;расскажем вам куда обращаться, для получения/приобретения
+				земельного участка и&nbsp;о&nbsp;необходимых документах для получения разрешения на&nbsp;проектирование
+				и&nbsp;строительство собственного дома.</p>
+			<video class="left" src="a1-2.mp4" autoplay loop></video>
+			<p>1) <b>Убедитесь что вы&nbsp;не&nbsp;полностью банкрот,</b> так как сроку от&nbsp;получения участка
+				до&nbsp;окончания строительства отводится всего-ничего 8&nbsp;лет (если замораживать
+				и&nbsp;размораживать строительный процесс), следовательно закрома нужны не&nbsp;пустые. Если с&nbsp;этим
+				всё хорошо, едем дальше.</p>
+			<p>2) <b>Получение участка.</b> Земельный участок можно приобрести через аукцион, в&nbsp;некоторых случаях
+				можно получить право на&nbsp;покупку участка без аукциона либо стать в&nbsp;очередь на&nbsp;его
+				получение. Вам следует обратиться в&nbsp;землеустроительную службу местного исполкома, с&nbsp;заявлением
+				на&nbsp;получение/на регистрацию земельного участка. Однако следует иметь ввиду: в&nbsp;течение одного
+				года со&nbsp;дня регистрации земельного участка, предоставленного для строительства жилого дома,
+				вы&nbsp;обязаны приступить к&nbsp;его освоению, и&nbsp;для этого недостаточно поставить забор.
+				За&nbsp;первый год нужно как минимум устроить фундамент. А&nbsp;в&nbsp;течение трёх лет&nbsp;&mdash;
+				возвести коробку под крышу. В&nbsp;противном случае, земельный участок подлежит изъятию. Это
+				не&nbsp;значит, что участок начнут изымать едва&nbsp;ли не&nbsp;за&nbsp;один час просроченного времени,
+				но&nbsp;если фундамент стоит пять лет и&nbsp;больше никаких действий не&nbsp;наблюдается,
+				то&nbsp;в&nbsp;судебном порядке земля может изыматься и&nbsp;выставляться на&nbsp;аукцион.</p>
+			<p>Стоит отметить, что вышесказанное зависит от&nbsp;способа получения участка и&nbsp;справедливо
+				не&nbsp;во&nbsp;всех случаях. Подробнее о&nbsp;получении участка можно почитать <a
+					href="http://www.samsebeyurist.by/zhile-i-nedvizhimost/zemelnyy-uchastok-pod-stroitelstvo-doma"
+					target="_blank">здесь</a>.</p>
+			<figure>
+				<img class="center" src="a1-3.jpg" loading="lazy"
+					alt="Земельные участки, отводимые под строительство индивидуальных жилых домов">
+				<figcaption>Земельные участки, отводимые под строительство индивидуальных жилых домов</figcaption>
+			</figure>
+			<p>Результатом посещения инстанций должно быть наличие свидетельства о&nbsp;государственной регистрации
+				земельного участка.</p>
+			<p>3) <b>Получение разрешения на&nbsp;проектирование и&nbsp;строительство.</b>
+				Проектирование и&nbsp;возведение жилого дома осуществляются только на&nbsp;основании разрешительной
+				документации, значит придется ее&nbsp;раздобыть. Перечень включает в&nbsp;себя:</p>
+			<ul>
+				<li>решение исполкома о&nbsp;разрешении проведения проектно-изыскательских работ и&nbsp;строительства
+					одноквартирного, блокированного жилого дома&nbsp;и (или) нежилых капитальных построек
+					на&nbsp;придомовой территории;</li>
+				<li>архитектурно-планировочное задание;</li>
+				<li>заключения согласующих организаций;</li>
+				<li>технические условия на&nbsp;инженерно-техническое обеспечение объекта.</li>
+			</ul>
+			<p>Все перечисленные документы вам предоставляет местный исполком в&nbsp;течение одного месяца после подачи
+				соответствующего заявления. Подробнее можно узнать по&nbsp;<a
+					href="http://www.samsebeyurist.by/zhile-i-nedvizhimost/stroitelstvo-chastnogo-zhilogo-doma#part1"
+					target="_blank">ссылке</a>.</p>
+			<div class="line"></div>
+			<p><a href="a2.php">Вторая часть</a>, <a href="a3.php">третья часть</a>.</p>
+			<p>Читайте наши статьи на&nbsp;сайте и&nbsp;в&nbsp;нашей группе <a href="https://vk.com/marchukby"
+					target="_blank">&laquo;MARCHUK&nbsp;| Строительные проекты&raquo;</a>. Подписывайтесь, чтобы
+				не&nbsp;пропустить полезный материал по&nbsp;теме проектирования и&nbsp;строительства.</p>
+		</article>
+	</main>
+
+    <?php include_once '../footer.php'?>
+
+	<script src="../js/script.js"></script>
+
+</body>
+
+</html>
