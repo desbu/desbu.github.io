9d145362e5b3c6399004dddece1dfc47c4e7a722
desbu <55109922+desbu@users.noreply.github.com>
2026-05-12 19:33:34 +0300

    Revert "Refactoring"
    This reverts commit 2330129a57e9ea3493903bc7d56ad487d03b0c99.

--- index.php
+++ index.php
@@ -28,14 +28,15 @@
     <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 </head>
 
-<body>
+<body class="main-page">
 <header id="top">
     <nav>
-        <h1 class="nav-logo">
-            <span class="hidden-logo" itemprop="name">MARCHUK.BY | </span>Строительные проекты
-        </h1>
+        <a class="nav-logo">
+            <h1><span class="hidden-logo" itemprop="name">MARCHUK.BY | </span>Строительные проекты</h1>
+        </a>
         <ul class="main-nav">
             <li class="link"><a>Главная</a></li>
+            <div class="line-nav"></div>
             <li class="link"><a href="#about-us">О нас</a></li>
             <li class="link"><a href="#services">Услуги</a></li>
             <li class="link"><a href="#reviews">Отзывы</a></li>
